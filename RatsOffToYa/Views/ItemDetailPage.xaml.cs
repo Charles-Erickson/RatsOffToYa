153a2981e35037463f18294e1921f8f102db68cd
Charles-Erickson <charles.erickson414@gmail.com>
2021-05-29 18:16:16 -0500

    Initial commit

---
+++ RatsOffToYa/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RatsOffToYa.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RatsOffToYa.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ RatsOffToYa/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using RatsOffToYa.Models;
+using RatsOffToYa.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RatsOffToYa.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
